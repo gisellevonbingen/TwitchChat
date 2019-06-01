34b4d729ed0ac208565c9e60a435d9053ea89126
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-01 23:44:59 +0900

    Update StringUtils.cs

--- TwitchChat/StringUtils.cs
+++ TwitchChat/StringUtils.cs
@@ -8,84 +8,99 @@ namespace TwitchChat
 {
     public static class StringUtils
     {
-        public static string RemoveSuffx(string text, char suffix)
+        public static string[] Split(this string str, string separator)
         {
-            return RemoveSuffx(text, suffix.ToString());
+            return str.Split(new string[] { separator }, StringSplitOptions.None);
         }
 
-        public static string RemoveSuffx(string text, string suffix)
+        public static string[] Split(this string str, string separator, StringSplitOptions options)
         {
-            if (text == null)
+            return str.Split(new string[] { separator }, options);
+        }
+
+        public static string[] Split(this string str, string separator, int count, StringSplitOptions options)
+        {
+            return str.Split(new string[] { separator }, count, options);
+        }
+
+        public static string RemoveSuffx(this string str, char suffix)
+        {
+            return RemoveSuffx(str, suffix.ToString());
+        }
+
+        public static string RemoveSuffx(this string str, string suffix)
+        {
+            if (str == null)
             {
                 return null;
             }
 
-            if (text.EndsWith(suffix) == true)
+            if (str.EndsWith(suffix) == true)
             {
-                return text.Substring(0, text.Length - suffix.Length);
+                return str.Substring(0, str.Length - suffix.Length);
             }
 
-            return text;
+            return str;
         }
 
-        public static string AddSuffx(string text, char suffix)
+        public static string AddSuffx(this string str, char suffix)
         {
-            return AddSuffx(text, suffix.ToString());
+            return AddSuffx(str, suffix.ToString());
         }
 
-        public static string AddSuffx(string text, string suffix)
+        public static string AddSuffx(this string str, string suffix)
         {
-            if (text == null)
+            if (str == null)
             {
                 return null;
             }
 
-            if (text.EndsWith(suffix) == false)
+            if (str.EndsWith(suffix) == false)
             {
-                return text + suffix;
+                return str + suffix;
             }
 
-            return text;
+            return str;
         }
 
-        public static string RemovePrefix(string text, char prefix)
+        public static string RemovePrefix(this string str, char prefix)
         {
-            return RemovePrefix(text, prefix.ToString());
+            return RemovePrefix(str, prefix.ToString());
         }
 
-        public static string RemovePrefix(string text, string prefix)
+        public static string RemovePrefix(this string str, string prefix)
         {
-            if (text == null)
+            if (str == null)
             {
                 return null;
             }
 
-            if (text.StartsWith(prefix) == true)
+            if (str.StartsWith(prefix) == true)
             {
-                return text.Substring(prefix.Length);
+                return str.Substring(prefix.Length);
             }
 
-            return text;
+            return str;
         }
 
-        public static string AddPrefix(string text, char prefix)
+        public static string AddPrefix(this string str, char prefix)
         {
-            return AddPrefix(text, prefix.ToString());
+            return AddPrefix(str, prefix.ToString());
         }
 
-        public static string AddPrefix(string text, string prefix)
+        public static string AddPrefix(this string str, string prefix)
         {
-            if (text == null)
+            if (str == null)
             {
                 return null;
             }
 
-            if (text.StartsWith(prefix) == false)
+            if (str.StartsWith(prefix) == false)
             {
-                return prefix + text;
+                return prefix + str;
             }
 
-            return text;
+            return str;
         }
 
     }
