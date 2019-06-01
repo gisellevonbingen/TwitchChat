7e6c6d453917fd70f2f4cf18a64b2f7aba452cf8
gisellevonbingen <44163945+gisellevonbingen@users.noreply.github.com>
2019-06-01 23:45:28 +0900

    Create EnumerableUtils.cs

---
+++ TwitchChat/EnumerableUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchChat
+{
+    public static class EnumerableUtils
+    {
+        public static T[] ToArray<T>(this IEnumerable<T> enumerable, Func<T, bool> func)
+        {
+            return enumerable.Where(func).ToArray();
+        }
+
+    }
+
+}
